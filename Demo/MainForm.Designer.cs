088e7550dc1852b04a36292216b8f0a6823da233
katebi.milad <katebi.milad@yahoo.com>
2020-11-12 12:03:39 +1300

    Add project files.

---
+++ Demo/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Demo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MyButton = new System.Windows.Forms.Button();
+            this.OutputTB = new System.Windows.Forms.TextBox();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MyButton
+            // 
+            this.MyButton.Location = new System.Drawing.Point(23, 22);
+            this.MyButton.Name = "MyButton";
+            this.MyButton.Size = new System.Drawing.Size(141, 71);
+            this.MyButton.TabIndex = 0;
+            this.MyButton.Text = "My Button";
+            this.MyButton.UseVisualStyleBackColor = true;
+            this.MyButton.Click += new System.EventHandler(this.MyButton_Click);
+            // 
+            // OutputTB
+            // 
+            this.OutputTB.Location = new System.Drawing.Point(262, 38);
+            this.OutputTB.Name = "OutputTB";
+            this.OutputTB.Size = new System.Drawing.Size(196, 20);
+            this.OutputTB.TabIndex = 1;
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(259, 22);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(39, 13);
+            this.OutputLabel.TabIndex = 2;
+            this.OutputLabel.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "/";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(262, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(262, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Num1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Num2";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.OutputTB);
+            this.Controls.Add(this.MyButton);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MyButton;
+        private System.Windows.Forms.TextBox OutputTB;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Demo/MainForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, EventArgs e)
+        {
+            OutputTB.Text = "Hello World from Button";
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            OutputTB.Text = "Hello World from Form";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int var1 = Convert.ToInt32(textBox1.Text);
+            int var2 = int.Parse(textBox2.Text);
+
+            OutputTB.Text = var1 + var2 + "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double var1 = Convert.ToInt32(textBox1.Text);
+            double var2 = Convert.ToInt32(textBox2.Text);
+            double var3 = var1 / var2;
+
+            var3 = Math.Round(var3, 1);
+
+            OutputTB.Text = var3 + "";
+        }
+    }
+}
